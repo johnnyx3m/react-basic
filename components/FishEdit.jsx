95dcfc6bc68a639613bd63a5ed90638c1360b9f0
irrailyn <ilyn@idearobin.com>
2016-11-29 10:47:36 +0800

    Correctly load values for fish status and image

--- components/FishEdit.jsx
+++ components/FishEdit.jsx
@@ -6,12 +6,12 @@ export default class Fish extends Component {
             <div className="fish-edit">
                 <input type="text" ref="fishName" placeholder="Fish Name" value={this.props.data.fishName} />
                 <input type="number" ref="fishPrice" placeholder="Fish Price" value={this.props.data.fishPrice} />
-                <select ref="status">
+                <select ref="status" value={this.props.data.status}>
                     <option value="available">Fresh!</option>
                     <option value="unavailable">Sold Out!</option>
                 </select>
                 <textarea type="text" ref="fishDescription" placeholder="Desc" value={this.props.data.fishDescription}></textarea>
-                <input type="text" ref="imageUrl" placeholder="URL to Image" src={this.props.data.imageUrl} />
+                <input type="text" ref="imageUrl" placeholder="URL to Image" value={this.props.data.imageUrl} />
                 <button>Remove Fish</button>
             </div>
         )
